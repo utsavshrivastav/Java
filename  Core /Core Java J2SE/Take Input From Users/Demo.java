9e010c05a2e5ea1e4a6a5ac6107e146f3ca6bb81
utsavshrivastav <thevickyshrivastav@gmail.com>
2024-11-03 13:23:02 +0530

    add files

---
+++  Core /Core Java J2SE/Take Input From Users/Demo.java
@@ -0,0 +1,17 @@
+/* 
+import java util.Scanner;
+class Demo
+{
+    public static void main(String arg[])
+    {
+        int a , b , c ;
+        Scanner ip = new Scanner(System.in);
+        System.out.println("Enter a and b");
+        a = ip.nextInt();
+        b = ip.nextInt();
+        c = a+b;
+        System.out.println("sum"+c);
+
+    }
+}
+*/
