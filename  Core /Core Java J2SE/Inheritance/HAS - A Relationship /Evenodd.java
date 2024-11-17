11613a73e143b449f52ab236b07bedc9d62e7d66
utsavshrivastav <thevickyshrivastav@gmail.com>
2024-11-17 23:29:16 +0530

    heirarchical Inheritance Files

---
+++  Core /Core Java J2SE/Inheritance/HAS - A Relationship /Evenodd.java
@@ -0,0 +1,16 @@
+
+
+public class Evenodd {
+    void check(int no)
+    {
+        if(no % 2 == 0)
+        {
+            System.out.println("Even no");
+        }
+        else{
+            System.out.println("Odd no ");
+        }
+    }
+    }
+
+    
---
+++  Core /Core Java J2SE/Inheritance/Heirarchical Inheritance/Final Keyword/SuperClass.java
@@ -0,0 +1,26 @@
+/* 
+public class SuperClass
+{
+    final void show()
+    {
+        System.out.println("Sper class ");
+    }
+}
+ 
+ class SubClass extends SuperClass
+ {
+    @Override      /* cannot overide because final keyword is insert .  */
+   /*  void show()
+    {
+        System.out.println("Sub classes");
+    }
+}
+
+class fin {
+    public static void main()
+    {
+        SubClass ob = new SubClass();
+        ob.show();
+    }
+} */
+
