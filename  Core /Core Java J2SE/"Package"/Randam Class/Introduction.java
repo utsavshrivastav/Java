dbb5e5d11a95a7d15f17238aa12d4921bc9e14bf
utsavshrivastav <thevickyshrivastav@gmail.com>
2024-11-21 18:58:33 +0530

    Complete Package

---
+++  Core /Core Java J2SE/"Package"/Randam Class/Introduction.java
@@ -0,0 +1,8 @@
+/*
+ 
+1. Randaom Package import if Randam no generate .
+
+2. To Genererate Random Number Using NextInt Method . we have to import Random Class of util Package 
+          [ie.. rn.nextInt(no)]
+           (n-1) - means yeah tak no generate honge . ie. (n-1)tak number generate honge  
+ */
